c13b29b86e336660cbe94b41f68bbe7cb1409b49
Caiwei <sun_don@qq.com>
2025-03-01 23:30:06 +0800

    create environment

--- autocruise.py
+++ autocruise.py
@@ -1,7 +1,17 @@
-import numpy as np
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+def autocruise(matrix):
+    # 创建网格对象
+    grid = Grid(matrix=matrix)
 
-class AutoCruise():
-    pass
+    # 设置起点和终点
+    start = grid.node(0, 0)
+    end = grid.node(9, 5)
 
-controller = AutoCruise()
-controller.show()
+    # 创建A*寻路器
+    finder = AStarFinder()
+
+    # 寻路并获取路径
+    path, _ = finder.find_path(start, end, grid)
+    
+    return path
